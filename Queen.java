54d35adb216f713845c9d94c1f9b304af5a1adcd
1cek4 <67841585+1cek4@users.noreply.github.com>
2025-10-27 22:06:00 -0600

    Initial commit

---
+++ Queen.java
@@ -0,0 +1,15 @@
+public class Queen {
+     int[] position;
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    public Queen(int[] positon) {
+        this.position = positon;
+    }
+}
