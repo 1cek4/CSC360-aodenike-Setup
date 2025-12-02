42029a5faccd7ae392d1cbe551b8ec786472b323
1cek4 <67841585+1cek4@users.noreply.github.com>
2025-12-01 19:21:21 -0700

    chore: remove compiled .class files and add .gitignore

--- Main.java
+++ Main.java
@@ -5,7 +5,10 @@ public class Main{
     public static void main(String[] args) {
         int n = 4;
 
-        NQueensSolver solver = SolverFactory.makeSolver(SolverFactory.SolverType.BACKTRACKING);
+        // Choose solver from args: "bitmask" will use the bitmasking solver, otherwise default to backtracking.
+        NQueensSolver solver = SolverFactory.makeSolver(
+            (args.length > 0 && "bitmask".equalsIgnoreCase(args[0])) ? SolverFactory.SolverType.BITMASKING : SolverFactory.SolverType.BACKTRACKING
+        );
 
         ArrayList<ArrayList<String>> solutions = solver.nQueenStarter(n);
         System.out.println(solutions);
--- SolverFactory.java
+++ SolverFactory.java
@@ -1,11 +1,15 @@
 public class SolverFactory {
     public enum SolverType {
-        BACKTRACKING
+        BACKTRACKING,
+        BITMASKING,
+        CONSTRAINT_PROPAGATION
     }
     public static NQueensSolver makeSolver(SolverType type){
         switch(type){
             case BACKTRACKING:
                 return new BacktrackSolver();
+            case BITMASKING:
+                return new BitmaskingSolver();
             default:
                 throw new IllegalArgumentException("Invalid solver type");
         }
